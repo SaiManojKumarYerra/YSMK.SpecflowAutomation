466d51fb7eaf22567b3a1c003218822ded6088f8
ysmk <yerrasaimanojkumar@outlook.com>
2023-01-17 18:55:15 +0530

    Code Refactoring

--- YSMK.SpecflowAutomation/Base/Browser.cs
+++ YSMK.SpecflowAutomation/Base/Browser.cs
@@ -5,20 +5,12 @@ namespace YSMK.SpecflowAutomation.Base
 {
     public class Browser
     {
-        private readonly IWebDriver _driver;
+        //private readonly IWebDriver _driver;
 
-        public Browser(IWebDriver driver)
-        {
-            _driver = driver;
-        }
-
-        public BrowserType Type { get; set; }
-
-        public void GoToUrl(string url)
-        {
-            DriverContext.Driver.Url = url;
-            DriverContext.Driver.WaitForPageLoaded();
-        }
+        //public Browser(IWebDriver driver)
+        //{
+        //    _driver = driver;
+        //}
 
     }
 
